07bc9fe9724c776598979793339df8fa89a8fd85
OscarAyalaa <oscar.manuel.ayala.andrade@gmail.com>
2024-11-05 13:28:55 -0600

    agregar proyecto 1

---
+++ src/main/java/com/soa/V2FullChroniclesApplication.java
@@ -0,0 +1,13 @@
+package com.soa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V2FullChroniclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V2FullChroniclesApplication.class, args);
+	}
+
+}
